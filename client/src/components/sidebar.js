1b4e731becf0473cd2509b57c0e8a61263765696
Jaden Lemmon <jaden@jadenlemmon.com>
2021-01-26 21:34:55 -0600

    save

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { AiFillDelete } from 'react-icons/ai';
 import {
   Block,
   Card,
   Text,
   Button,
   Flex,
+  Clickable,
 } from '@actovos-consulting-group/ui-core';
 import styled from 'styled-components';
 import Avatar from './avatar';
@@ -26,11 +28,18 @@ const Container = styled(Block)`
 
 const ItemContainer = styled(Card)`
   box-shadow: 0px 0px 7px #a7c2cc;
+  position: relative;
 `;
 
 const Logo = styled.img`
   margin: 0 auto;
   display: block;
+`;
+
+const DeleteIcon = styled(Clickable)`
+  position: absolute;
+  right: 5px;
+  bottom: 5px;
 `;
 
 const urlPrefix = process.env.REACT_APP_API_URL || window.location.origin;
@@ -51,6 +60,9 @@ const ShipItem = ({ name, captain, avatar }) => {
           </div>
         </Block>
       </Flex>
+      <DeleteIcon onClick={() => null}>
+        <AiFillDelete color="#ccc" />
+      </DeleteIcon>
     </ItemContainer>
   );
 };
